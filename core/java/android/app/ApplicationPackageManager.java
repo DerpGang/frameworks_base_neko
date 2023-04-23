f7625b0e160ed7f7a6f6e44007205c9e4d97e006
Shivan Singh <shivan.0972@gmail.com>
2023-04-23 08:02:13 +0800

    core: Integrate and Enable Pixel features capabilities
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -861,6 +861,8 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.photos.trust_debug_certs",
             "com.google.android.feature.AER_OPTIMIZED",
             "com.google.android.feature.NEXT_GENERATION_ASSISTANT",
+            "com.google.android.feature.ADAPTIVE_CHARGING",
+            "com.google.android.feature.DREAMLINER",
             "android.software.game_service",
             "com.google.android.feature.EXCHANGE_6_2",
             "com.google.android.apps.dialer.call_recording_audio",
@@ -868,15 +870,13 @@ public class ApplicationPackageManager extends PackageManager {
     };
 
     private static final String[] featuresTensor = {
-            "com.google.android.feature.PIXEL_2025_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2025_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
             "com.google.android.feature.PIXEL_2022_EXPERIENCE",
             "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_EXPERIENCE"
+            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
     };
 
     private static final String[] featuresNexus = {
